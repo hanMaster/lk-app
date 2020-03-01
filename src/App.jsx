5923dc9ad8720c07fab899e25f7edf59003e00da
hanMaster <w54661c@gmail.com>
2020-03-01 22:30:31 +1000

    Befire eject

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import AppHeader from './components/header';
+import TemporaryDrawer from './components/drawer';
+
+function App() {
+  return (
+    <div className="App">
+      <AppHeader />
+      <TemporaryDrawer />
+    </div>
+  );
+}
+
+export default App;
